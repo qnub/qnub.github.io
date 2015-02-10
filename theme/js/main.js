648bb2887205cf62c5a9be75d0f90a67c97ef9b3
Vadim Lopatyuk <qnub.ru@gmail.com>
2015-02-10 12:41:31 +0600

    Generate Pelican site

---
+++ theme/js/main.js
@@ -0,0 +1,29 @@
+'use strict';
+
+$( function(){
+  $( '.article__content img' ).each(function(){
+    var $this = $(this);
+    $this.wrap('<a href="' + $this.attr('src') + '" rel="lightbox"></a>');
+  });
+
+  $('[rel="lightbox"]').fluidbox();
+
+  // var showVK = function(){
+
+  //   VK.Widgets.Group(
+  //     "vk_groups",
+  //     {
+  //       mode: 2,
+  //       width: 220,
+  //       height: 400,
+  //       color1: 'FFFFFF',
+  //       color2: '2B587A',
+  //       color3: '5B7FA6'
+  //     },
+  //     48255349
+  //   );
+  // }
+
+  // showVK();
+
+});
